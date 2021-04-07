954f82eea0999102c949c2ea77285f9623afe6b6
Uttam <kr.uttam1997@gmail.com>
2021-04-07 17:28:06 +0530

    Added actions-item img using src

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            price : 999,
+            title : 'Phone',
+            qty : 1,
+            img : ''
+        }
+    }
     render() {
+        const {price, title, qty} = this.state;
         return(
             <div className="cart-item">
                 <div className="left-block">
@@ -9,11 +19,14 @@ class CartItem extends React.Component {
                 </div>
 
             <div className="right-block">
-                <div>Phone</div>
-                <div>Rs 999</div>
-                <div>Qty : 1</div>
+                <div>{title}</div>
+                <div>{price}</div>
+                <div>{qty}</div>
                 <div className="cart-item-action">
                     {/* Button */}
+                    <img alt="increase" className="action-icons" src="https://t4.ftcdn.net/jpg/01/07/62/07/240_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" />
+                    <img alt="Decrease" className="action-icons" src="https://t3.ftcdn.net/jpg/03/73/49/86/240_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" />
+                    <img alt="Delete" className="action-icons" src="https://t3.ftcdn.net/jpg/02/58/95/14/240_F_258951445_Md7JDFw3Qj4LPTorWUYdImTzS49fSnOf.jpg" />
                 </div>
             </div>
             </div>
