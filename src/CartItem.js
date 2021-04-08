29d0e5d1361da6fc03ec67d9f4fecbed665e88dc
Uttam <kr.uttam1997@gmail.com>
2021-04-08 13:37:17 +0530

    Add Cart Component

--- src/CartItem.js
+++ src/CartItem.js
@@ -10,6 +10,40 @@ class CartItem extends React.Component {
             img : ''
         }
     }
+
+    increaseQuantity = () => {
+        //we will use setState to update the object of the component and render it on screen 
+        //we will use this setState form when we don't have to use the previous state value
+        // this.setState({
+        //     qty : this.state.qty + 1;
+        // })
+
+        //setState form 2 - if prevState require use this
+        this.setState((prevState) => {
+            return{
+                qty : prevState.qty + 1
+            }
+        }, () => {
+            console.log('this.state', this.state);
+        });
+    }
+
+    decreaseQuantity = () => {
+
+        const {qty} = this.state;
+
+        if(qty === 0){
+            return;
+        }
+
+        this.setState((prevState) => {
+            return {
+                qty : prevState.qty - 1
+            }
+        });
+    }
+
+
     render() {
         const {price, title, qty} = this.state;
         return(
@@ -24,9 +58,25 @@ class CartItem extends React.Component {
                 <div>{qty}</div>
                 <div className="cart-item-action">
                     {/* Button */}
-                    <img alt="increase" className="action-icons" src="https://t4.ftcdn.net/jpg/01/07/62/07/240_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" />
-                    <img alt="Decrease" className="action-icons" src="https://t3.ftcdn.net/jpg/03/73/49/86/240_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" />
-                    <img alt="Delete" className="action-icons" src="https://t3.ftcdn.net/jpg/02/58/95/14/240_F_258951445_Md7JDFw3Qj4LPTorWUYdImTzS49fSnOf.jpg" />
+                    <img 
+                        alt="increase" 
+                        className="action-icons" 
+                        src="https://t4.ftcdn.net/jpg/01/07/62/07/240_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" 
+                        onClick = {this.increaseQuantity}
+                        />
+                    
+                    <img 
+                        alt="Decrease" 
+                        className="action-icons" 
+                        src="https://t3.ftcdn.net/jpg/03/73/49/86/240_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" 
+                        onClick = {this.decreaseQuantity}
+                        />
+                    
+                    <img 
+                        alt="Delete" 
+                        className="action-icons" 
+                        src="https://t3.ftcdn.net/jpg/02/58/95/14/240_F_258951445_Md7JDFw3Qj4LPTorWUYdImTzS49fSnOf.jpg" 
+                        />
                 </div>
             </div>
             </div>
